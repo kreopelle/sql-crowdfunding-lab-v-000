d0f16d26dc736bdfdf8fc318969ef5c2e10de35e
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 14:45:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,10 +8,10 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "
-SELECT projects.titles, SUM(pledges.amount) FROM projects
+SELECT projects.title, SUM(pledges.amount) FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
-GROUP BY (projects.titles);
+GROUP BY (projects.title);
 "
 end
 
